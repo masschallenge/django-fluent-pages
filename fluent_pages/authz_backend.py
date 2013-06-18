31b10e5a5ca1ca0bf2b1ec8ee0543c4f4bf44352
cewing <cris@crisewing.com>
2013-06-17 18:24:11 -0700

    begin work on a basic authz backend that allows access to all pages for all users.

---
+++ fluent_pages/authz_backend.py
@@ -0,0 +1,14 @@
+class SimpleBackend(object):
+
+    def pages_for_user(self, user_obj):
+        """returns a list of IDs for UrlNodes the user can see
+
+        This simple implementation assumes all user can see all UrlNodes
+        """
+        from fluent_pages.models import UrlNode
+        if not hasattr(user_obj, '_fluent_pages_node_cache'):
+            # build the cache of pages this user can see
+            all_pks = tuple(UrlNode.objects.values_list('pk', flat=True))
+            user_obj._fluent_pages_node_cache = all_pks
+        return user_obj._fluent_pages_node_cache
+        
